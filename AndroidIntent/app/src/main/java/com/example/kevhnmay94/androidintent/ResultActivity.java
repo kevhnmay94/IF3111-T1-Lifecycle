bd0fd263368121d5d999bed3315fb4744c2a276a
kevhnmay94 <kevhnmay94@gmail.com>
2015-02-07 21:40:46 +0700

    Android Life Cycle tutorial: done

---
+++ AndroidIntent/app/src/main/java/com/example/kevhnmay94/androidintent/ResultActivity.java
@@ -0,0 +1,34 @@
+package com.example.kevhnmay94.androidintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Asus on 07/02/2015.
+ */
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+        Bundle extras = getIntent().getExtras();
+        String inputString = extras.getString("yourkey");
+        TextView view = (TextView) findViewById(R.id.displayintentextra);
+        view.setText(inputString);
+    }
+
+    @Override
+    public void finish(){
+        Intent intent = new Intent();
+        EditText editText = (EditText) findViewById(R.id.returnValue);
+        String extra = editText.getText().toString();
+        intent.putExtra("returnkey", extra);
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+
+}
