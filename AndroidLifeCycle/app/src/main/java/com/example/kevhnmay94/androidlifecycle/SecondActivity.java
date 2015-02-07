1a262b63da5193c3dd840ad3c6fcf8cba9103ec3
kevhnmay94 <kevhnmay94@gmail.com>
2015-02-07 18:31:26 +0700

    Android Life Cycle tutorial done

---
+++ AndroidLifeCycle/app/src/main/java/com/example/kevhnmay94/androidlifecycle/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.kevhnmay94.androidlifecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
